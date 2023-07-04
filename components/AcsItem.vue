208fc40996bfb9cfa7fe52e6e549b46a49ec49da
Sergey Tikhomirov <tihomirov.pro@gmail.com>
2023-07-04 21:48:05 +0300

    fix

--- components/AcsItem.vue
+++ components/AcsItem.vue
@@ -7,36 +7,32 @@ const props = defineProps<{
 }>()
 
 const totalPriceAcs = useAllPricesAcs()
-const itemCounter = ref<number>(0)
 const selectedAcs = useSelectedAcs()
 
-watchEffect(() => {
-  if (selectedAcs.value[props.title]) itemCounter.value = selectedAcs.value[props.title]
+onMounted(()=> {
+  if (!selectedAcs.value[props.title]) selectedAcs.value[props.title] = 0
 })
 
 const oneItemPrice = computed(()=> {
   if (props.prices.length === 1) return props.prices[0]
-  else if (props.title === 'Колокольчик "МАЙ"') return selectOnePrice(itemCounter.value, props.prices, [5, 19, 29, 49])
-  else return selectOnePrice(itemCounter.value, props.prices, [3, 9, 29, 49])
+  else if (props.title === 'Колокольчик "МАЙ"') return selectOnePrice(selectedAcs.value[props.title], props.prices, [5, 19, 29, 49])
+  else return selectOnePrice(selectedAcs.value[props.title], props.prices, [3, 9, 29, 49])
 })
 
-function removeItem() {
-  if (itemCounter.value !== 0) {
-    itemCounter.value--
-    selectedAcs.value[props.title] = itemCounter.value
-    totalPriceAcs.value[props.id] = itemCounter.value * oneItemPrice.value 
-  }
+function change() {
+  totalPriceAcs.value[props.id] = selectedAcs.value[props.title] * oneItemPrice.value
 }
 
 function addItem() {
-  itemCounter.value++
-  selectedAcs.value[props.title] = itemCounter.value
-  totalPriceAcs.value[props.id] = itemCounter.value * oneItemPrice.value
+  selectedAcs.value[props.title]++
+  totalPriceAcs.value[props.id] = selectedAcs.value[props.title] * oneItemPrice.value
 }
 
-function change() {
-  selectedAcs.value[props.title] = itemCounter.value
-  totalPriceAcs.value[props.id] = itemCounter.value * oneItemPrice.value
+function removeItem() {
+  if (selectedAcs.value[props.title] !== 0) {
+    selectedAcs.value[props.title]--
+    totalPriceAcs.value[props.id] = selectedAcs.value[props.title] * oneItemPrice.value
+  }
 }
 </script>
 
@@ -49,7 +45,6 @@ function change() {
 
   .dop__controls-wrap
     .dop__minus(@click="removeItem") -
-    input.dop__counter.form-control.bell(v-if="selectedAcs[title]" type="number" v-model="selectedAcs[title]" @input="change")
-    input.dop__counter.form-control.bell(v-else type="number" v-model="itemCounter" @input="change")
+    input.dop__counter.form-control.bell(type="number" min="0" placeholder="0" v-model="selectedAcs[title]" @input="change")
     .dop__plus(@click="addItem") +
 </template>
